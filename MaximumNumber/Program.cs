af12e03f43ce1b1e256afb6df812982ecf7f8244
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-21 19:39:51 +0530

    [Priya] add . Maximum number check

---
+++ MaximumNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaximumNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum Number Check Using Generics!");
+        }
+    }
+}
